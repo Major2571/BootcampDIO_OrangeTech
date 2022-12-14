3d4cdbc2cae9a781609c738c5fb3fb9323831c7c
Major2571 <caroline.tacats62@gmail.com>
2022-12-14 02:25:32 -0300

    Update

--- PokedexJS/assets/js/main.js
+++ PokedexJS/assets/js/main.js
@@ -1,31 +1,32 @@
 const pokemonList = document.getElementById("pokemonList");
 
 function convertPokemonToLi(pokemon) {
-  return ` <div class="pokemon-card-container"> 
-    <div class="pokemon-card">  
-    <div class="pokemon-card-bg">  
-  <div class="bg-pokeball">
-  </div>
-  </div>
-            <span class="number">#001</span>
+  return `<div class="pokemon-card-container">
+            <div class="pokemon-card">
 
-            <h2 class="name">${pokemon.name}</h2>
-        
-            <div class="details">
+                <div class="pokemon-card-bg">
+                    <div class="bg-pokeball"></div>
+                </div>
 
-                <ol class="types">
-                    <li class="type">Grass</li>
-                    <li class="type">Poison</li>
-                </ol>
+                <span class="number">#001</span>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png" alt="${pokemon.name}">
+                <h2 class="name">${pokemon.name}</h2>
+
+                <div class="details">
+
+                    <ol class="types">
+                        <li class="type">Grass</li>
+                        <li class="type">Poison</li>
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png" alt="${pokemon.name}">
+
+                </div>
 
             </div>
-
-            </div>
-          </div>`;
+         </div> `;
 }
 
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
 });
