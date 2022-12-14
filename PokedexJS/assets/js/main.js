0a73b6d30454c79c48466a8da57449f904501bb3
Major2571 <caroline.tacats62@gmail.com>
2022-12-14 01:22:21 -0300

    Update

--- PokedexJS/assets/js/main.js
+++ PokedexJS/assets/js/main.js
@@ -1,18 +1,29 @@
 const pokemonList = document.getElementById("pokemonList");
 
 function convertPokemonToLi(pokemon) {
-  return `<li class="pokemon">  
+  return ` <div class="pokemon-card-container"> 
+    <div class="pokemon-card">  
+    <div class="pokemon-card-bg">  
+  <div class="bg-pokeball">
+  </div>
+  </div>
             <span class="number">#001</span>
-            <span class="name">${pokemon.name}</span>
+
+            <h2 class="name">${pokemon.name}</h2>
         
             <div class="details">
+
                 <ol class="types">
                     <li class="type">Grass</li>
                     <li class="type">Poison</li>
                 </ol>
+
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png" alt="${pokemon.name}">
+
             </div>
-          </li>`;
+
+            </div>
+          </div>`;
 }
 
 pokeApi.getPokemons().then((pokemons = []) => {
