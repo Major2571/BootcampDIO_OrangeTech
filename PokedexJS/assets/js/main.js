9783fe9de8ddb4d3187118ec688f7de9ece335b9
Major2571 <caroline.tacats62@gmail.com>
2022-12-15 01:42:15 -0300

    Update

--- PokedexJS/assets/js/main.js
+++ PokedexJS/assets/js/main.js
@@ -1,34 +1,27 @@
-const pokemonList = document.getElementById("pokemonList");
-
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`);
-}
 
 function convertPokemonToLi(pokemon) {
   return `<div class="pokemon-card-container">
-            <div class="pokemon-card">
+            <div class="pokemon-card ${pokemon.type}">
 
                 <div class="pokemon-card-bg">
                     <div class="bg-pokeball"></div>
                 </div>
 
-                <span class="number">#0${pokemon.order}</span>
-
+                <span class="number">#0${pokemon.number}</span>
                 <h2 class="name">${pokemon.name}</h2>
 
                 <div class="details">
-
                     <ol class="types">
-                        ${convertPokemonTypesToLi(pokemon.types).join('')}
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                     </ol>
-
-                    <img src="${pokemon.sprites.other.home.front_default}" alt="${pokemon.name}">
-
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
                 </div>
 
             </div>
          </div> `;
 }
+
+const pokemonList = document.getElementById("pokemonList");
 
 pokeApi.getPokemons().then((pokemons = []) => {
   pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
